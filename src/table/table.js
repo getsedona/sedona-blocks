2b8f4a872872760901923a88b121ff3a630a5e6e
Maxim Sofronov <imaxsof@gmail.com>
2018-10-22 12:11:54 +0300

    refactor(table)

--- src/table/table.js
+++ src/table/table.js
@@ -2,37 +2,41 @@
 //------------------------------------------------------------------------------
 
 window.sedona.table = function() {
-	if (document.querySelector('.table') !== null) {
-		let elements = document.querySelectorAll('.table__wrap');
+	if (document.querySelector('.table .table__wrap') === null) {
+		return;
+	}
 
-		Array.prototype.forEach.call(elements, function(el) {
-			if (!el.parentElement.classList.contains('table--js-init')) {
-				el.parentElement.classList.add('table--js-init');
+	const elements = document.querySelectorAll('.table__wrap');
 
-				if (el.scrollWidth > el.clientWidth) {
-					el.parentElement.classList.add('table--right-shadow');
-				}
+	Array.prototype.forEach.call(elements, function(el) {
+		if (el.parentElement.classList.contains('table--js-init')) {
+			return;
+		}
 
-				el.addEventListener('scroll', function() {
-					if (el.scrollWidth - el.scrollLeft <= el.clientWidth) {
-						el.parentElement.classList.remove('table--right-shadow');
-					}
+		el.parentElement.classList.add('table--js-init');
 
-					if (el.scrollWidth - el.scrollLeft > el.clientWidth) {
-						el.parentElement.classList.add('table--right-shadow');
-					}
+		if (el.scrollWidth > el.clientWidth) {
+			el.parentElement.classList.add('table--right-shadow');
+		}
 
-					if (el.scrollLeft <= 0) {
-						el.parentElement.classList.remove('table--left-shadow');
-					}
+		el.addEventListener('scroll', function() {
+			if (el.scrollWidth - el.scrollLeft <= el.clientWidth) {
+				el.parentElement.classList.remove('table--right-shadow');
+			}
 
-					if (el.scrollLeft > 0) {
-						el.parentElement.classList.add('table--left-shadow');
-					}
-				});
+			if (el.scrollWidth - el.scrollLeft > el.clientWidth) {
+				el.parentElement.classList.add('table--right-shadow');
+			}
+
+			if (el.scrollLeft <= 0) {
+				el.parentElement.classList.remove('table--left-shadow');
+			}
+
+			if (el.scrollLeft > 0) {
+				el.parentElement.classList.add('table--left-shadow');
 			}
 		});
-	}
+	});
 }
 
 sedona.table();
